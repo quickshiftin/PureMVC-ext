2b8d879c4811a793441ccabfe2ac00a6634f3e08
Nathan Nobbe <nnobbe@photobucket.com>
2009-01-29 22:33:17 -0700

    add a verbose option (not sure if it works yet..)

--- cleantestdir.sh
+++ cleantestdir.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+
+if [[ -n $q && "$1" -eq "-v" ]]
+then
+	isVerbose=1
+else
+	isVerbose=0
+fi
+cwd=`pwd`;
+
 if [ -d "tests" ]
 then
-	rm "tests/*.php" 2>/dev/null
-	rm "tests/*.log" 2>/dev/null
-	rm "tests/*.exp" 2>/dev/null
-	rm "tests/*.diff" 2>/dev/null
-	echo "the 'test' directory has been cleaned";
+	if [ $isVerbose ]
+	then
+		rm $cwd/tests/*.php
+		rm $cwd/tests/*.log
+		rm $cwd/tests/*.exp
+		rm $cwd/tests/*.diff
+		rm $cwd/tests/*.out
+	else
+		rm $cwd/tests/*.php 2>/dev/null
+		rm $cwd/tests/*.log 2>/dev/null
+		rm $cwd/tests/*.exp 2>/dev/null
+		rm $cwd/tests/*.diff 2>/dev/null
+		rm $cwd/tests/*.out 2>/dev/null
+	fi
+	echo "the 'tests' directory has been cleaned";
 	exit;
 fi
-echo "the 'test' directory does not exist";
+echo "the 'tests' directory does not exist";
