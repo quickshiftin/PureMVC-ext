3fbe9553bd3cd18d8d0825c287933ad61406bc7c
Nathan Nobbe <nnobbe@photobucket.com>
2009-05-10 01:46:46 -0600

    support another "name" source, which is the name of the concrete INotification class

--- tests/testlib/MyNotification.php
+++ tests/testlib/MyNotification.php
@@ -1,8 +1,21 @@
 <?php
 class MyNotification implements INotification {
-	public function getName() {
-		return 'testNotification';
+	static private $shouldOverrideName = true;
+
+	public static function dissableNameOverride() {
+		self::$shouldOverrideName = false;
 	}
+
+	public static function enableNameOverride() {
+		self::$shouldOverrideName = true;
+	}
+
+	public function getName() {
+		if(self::$shouldOverrideName)
+			return 'testNotification';
+		return __CLASS__;
+	}
+
 	public function getType() {}
 	public function getBody() {}
 	public function setName($name) {}
