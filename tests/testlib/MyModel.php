2fbd8a7595e0cb783388f44db2388099c4cae23c
Nathan Nobbe <nnobbe@photobucket.com>
2009-05-10 11:01:55 -0600

    provide an option to compose MyProxy (rather than Proxy [the default]); remove pointless retrieveProxy override

--- tests/testlib/MyModel.php
+++ tests/testlib/MyModel.php
@@ -1,9 +1,23 @@
 <?php
 class MyModel extends Model {
+	private static $shouldUseMyProxy = false;
+
 	public static function getInstance() {
 		if(is_null(self::$instance))
 			self::$instance = new self();
 		return self::$instance;
+	}
+
+	public static function useMyProxy() {
+		self::$shouldUseMyProxy = true;
+	}
+
+	public function initializeModel() {
+		if(self::$shouldUseMyProxy) {
+			$p = new MyProxy();
+			$this->proxyMap[$p->getProxyName()] = $p;
+			var_dump($this);
+		}
 	}
 
 	public function hasProxy($notificationName) {
@@ -12,10 +26,6 @@ class MyModel extends Model {
 
 	public function registerProxy($proxy) {
 		var_dump($proxy);
-	}
-
-	public function retrieveProxy($notificationName) {
-		echo $notificationName . PHP_EOL;
 	}
 
 	public function removeProxy($notificationName) {
