db9af94153d4e999b3ac5c03020a53dc3429c194
Nathan Nobbe <nnobbe@photobucket.com>
2009-05-15 21:05:28 -0600

    some tweaks to MyView while working on the view unit tests

--- tests/testlib/MyView.php
+++ tests/testlib/MyView.php
@@ -1,6 +1,7 @@
 <?php
 class MyView extends View {
 	public static $isVerbose = false;
+	public static $showName = false;
 
 	public static function getInstance() {
 		if(is_null(self::$instance))
@@ -12,30 +13,41 @@ class MyView extends View {
 		self::$isVerbose = true;
 	}
 
+	public static function showName() {
+		self::$showName = true;
+	}
+
 	public function hasMediator($notificationName) {
 		echo $notificationName . PHP_EOL;
+		return parent::hasMediator($notificationName);
 	}
 
 	public function registerMediator($mediator) {
-		var_dump($mediator);
+		if(self::$isVerbose)
+			var_dump($mediator);
+		return parent::registerMediator($mediator);
 	}
 
 	public function registerObserver( $notificationName, IObserver $observer ) {
 		if(self::$isVerbose)
 			var_dump(func_get_args());
+		if(self::$showName)
+			echo "notificationName: $notificationName" . PHP_EOL;
 		parent::registerObserver($notificationName, $observer);
 	}
 
 	public function retrieveMediator($notificationName) {
 		echo $notificationName . PHP_EOL;
+		return parent::retrieveMediator($notificationName);
 	}
 
 	public function removeMediator($notificationName) {
 		echo $notificationName . PHP_EOL;
+		return parent::removeMediator($notificationName);
 	}
 
 	public function notifyObservers($inotification) {
-echo 'CARE' . PHP_EOL;
 		echo get_class($inotification) . PHP_EOL;
+		return parent::notifyObservers($inotification);
 	}
 }
